7a5bdd5d2b24314a959a5dcf8b2a631b9a815fc6
Ricardo Alejandro <rjalejandrob@gmail.com>
2024-02-09 23:56:41 +0100

    keep trying with the profile picture

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -186,7 +186,7 @@ let transTheme = () => {
 let initTheme = (theme) => {
   if (theme == null || theme == "null") {
     const userPref = window.matchMedia;
-    if (userPref && userPref("(prefers-color-scheme: dark)").matches) {
+    if ((userPref && userPref("(prefers-color-scheme: dark)").matches) || userPref('(max-width: 768px)').matches) {
       theme = "dark";
     }
   }
